65675b106cc3b9f0377de4b76ae7ca942b499945
Olivier Butler <olivier.butler@gmail.com>
2018-09-28 12:02:49 +0100

    Added multiplyer to geteger, needs work since only pushes problem of integer size down the line a few orders of magnitude

--- Massive Integer Machine.playground/Contents.swift
+++ Massive Integer Machine.playground/Contents.swift
@@ -14,11 +14,11 @@ struct Geteger {
         }
     }
     
-    init(number: Int) {
+    init(number: Int = 0) {
         self.add(number)
-        print("Initialised Geteger ", stringValue)
     }
     
+    // TODO: Rewrite using <T>().enumerated().map { (index: Int, element: <T>) in ... }
     mutating func add(_ number: Int) {
         let arrayInput = String(number).flatMap{Int(String($0))!}
         let endIndex = arrayInput.count - 1
@@ -29,7 +29,7 @@ struct Geteger {
             if self.arrayValue.count - 1 >= index {
                 self.arrayValue[index] += toAdd
             } else {
-                self.arrayValue.insert(toAdd, at: index)
+                self.arrayValue.append(toAdd)
             }
             if self.arrayValue[index] > 9 {
                 self.arrayValue[index] = Int(Float(arrayValue[index]).truncatingRemainder(dividingBy: 10))
@@ -41,8 +41,43 @@ struct Geteger {
         }
     }
     
+    // TODO: Rewrite using <T>().enumerated().map { (index: Int, element: <T>) in ... }
+    mutating func multiply(by number: Int) {
+        let walkableNum = String(number).flatMap{Int(String($0))!}
+        let length = walkableNum.count - 1
+        var multiGet = Geteger(number: 0)
+        var walkableNumIndex = 0
+        while walkableNumIndex <= length {
+            let key = walkableNum[walkableNumIndex]
+            var toAdd = 0
+            for arrayValueIndex in 0..<self.arrayValue.count {
+                let chunk = (self.arrayValue[arrayValueIndex] * key * Int(pow(10, arrayValueIndex) as NSDecimalNumber))
+                // All chunks are the same size, just shifted. Need an add function that doesn't increace cost just because new col
+                print("Chunk", chunk)
+                toAdd += chunk
+            }
+            // You need to make sure toAdd doesn't become another massive int - some bit shifting maybe?
+            print("toAdd", toAdd)
+            multiGet.add(toAdd * Int(pow(10, length - walkableNumIndex) as NSDecimalNumber))
+            walkableNumIndex += 1
+        }
+        self.arrayValue = multiGet.arrayValue
+    }
+    
 }
 
-var myMachine = Geteger(number: 56779)
-myMachine.add(30997)
-print(myMachine.stringValue)
+//var myMachine = Geteger(number: 56779)
+//print("Initialised myMachine<Geteger>", myMachine.stringValue)
+//myMachine.add(30997)
+//print("+ 30997 =", myMachine.stringValue)
+//myMachine.multiply(by: 25)
+//print(". 25 =", myMachine.stringValue)
+
+var factorial = 20
+var factorialVictim = Geteger(number: factorial)
+for factorialIndex in 1..<factorial {
+    factorialVictim.multiply(by: factorial - factorialIndex)
+}
+print("\(factorial) != ", factorialVictim.stringValue)
+
+
