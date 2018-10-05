9ee60b7a65db02d80f66135e39c5a398520045b0
Olivier Butler <olivier.butler@gmail.com>
2018-10-05 12:45:23 +0100

    Improved leaderboard algo with list, but still not fast enough, needs to be tree

--- Ranker.playground/Contents.swift
+++ Ranker.playground/Contents.swift
@@ -4,24 +4,43 @@ import Foundation
 
 func climbingLeaderboard(scores: [Int], alice: [Int]) -> [Int] {
     var output = [Int]()
-    for aliceScore in alice {
-        var rank = 0
-        var last = -1
-        for score in scores {
-            if score != last {
-                rank += 1
-            }
-            last = score
-            if score <= aliceScore {
-                output.append(rank)
-                break
-            }
-        }
-        if last == scores.last {
-            output.append(rank + 1)
+    var list: Node!
+    var tempNodeBuffer: Node? = nil
+    for score in scores {
+        if let previous = tempNodeBuffer {
+            tempNodeBuffer = Node(value: score, previous: previous)
+        } else {
+            list = Node(value: score, previous: nil)
+            tempNodeBuffer = list
         }
     }
+    for score in alice {
+        var position: Node = list
+        while score < position.value {
+            position = position.next ?? Node(value: -1, previous: tempNodeBuffer)
+        }
+        print(position.rank)
+        output.append(position.rank)
+    }
     return output
+}
+
+class Node {
+    
+    let value: Int
+    let rank: Int
+    var next: Node? = nil
+    
+    init(value: Int, previous: Node?) {
+        self.value = value
+        if let previous = previous {
+            self.rank = previous.value == value ? previous.rank : previous.rank + 1
+            previous.next = self
+        } else {
+            self.rank = 1
+        }
+    }
+    
 }
 
 print(climbingLeaderboard(scores: [100, 90, 90, 80, 70, 60, 32, 1], alice: [1, 0, 50, 44, 90]))
